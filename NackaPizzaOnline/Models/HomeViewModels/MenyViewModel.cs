cb177e29cc8458650a1b126249caf18c4726f5d3
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-24 11:39:32 +0200

    Building MenyViewModel 1.0

--- NackaPizzaOnline/Models/HomeViewModels/MenyViewModel.cs
+++ NackaPizzaOnline/Models/HomeViewModels/MenyViewModel.cs
@@ -7,5 +7,8 @@ namespace NackaPizzaOnline.Models.HomeViewModels
 {
     public class MenyViewModel
     {
+        public List<DishIngredient> DishIngredients { get; set; } = new List<DishIngredient>();
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+        public List<Category> Categories { get; set; } = new List<Category>();
     }
 }
