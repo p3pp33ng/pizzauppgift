bb61c43561847c9ed260e1b59d46abb98d00a9a1
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-24 11:28:14 +0200

    Build Order and OrderItems(orderitems maybe will become cartitems)

---
+++ NackaPizzaOnline/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NackaPizzaOnline.Models
+{
+    public enum PayMethods
+    {
+        CreditCard,
+        Invoice,
+        Paypal,
+        PayOnArrival
+    }
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public int TotalAmount { get; set; }
+        public ApplicationUser User { get; set; }
+        public bool Paid { get; set; }
+        public PayMethods PayMethod { get; set; }
+    }
+}
---
+++ NackaPizzaOnline/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NackaPizzaOnline.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>;
+        public Dish Dish { get; set; }
+        public int Price { get; set; }
+    }
+}
