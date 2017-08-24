a9bbbf349e34ea195ab2114b8a6df565918c0ba2
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-24 11:36:29 +0200

    Created a viewmodel for meny

--- NackaPizzaOnline/Controllers/HomeController.cs
+++ NackaPizzaOnline/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace NackaPizzaOnline.Controllers
         }
         public IActionResult Index()
         {
+
             return View();
         }
 
---
+++ NackaPizzaOnline/Models/HomeViewModels/MenyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NackaPizzaOnline.Models.HomeViewModels
+{
+    public class MenyViewModel
+    {
+    }
+}
