40c6e6caf9799a671b8e7adec3231d565b8fe3ba
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-25 09:30:20 +0200

    Getting modal up for picking ingredients.

---
+++ NackaPizzaOnline/wwwroot/js/Scripts/Home/IndexScript.js
@@ -0,0 +1,15 @@
+﻿var modal = document.getElementById('ingredientModal');
+var btn = document.getElementById('ingredientsbtn');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
