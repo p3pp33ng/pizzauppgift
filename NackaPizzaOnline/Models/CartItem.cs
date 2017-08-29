126c6c7e964751c31ea199570e0a562d7816f5a2
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-29 13:52:08 +0200

    Created CartItem

---
+++ NackaPizzaOnline/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NackaPizzaOnline.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public List<int> IngredientIds { get; set; } = new List<int>();
+        public int DishId { get; set; }
+    }
+}
