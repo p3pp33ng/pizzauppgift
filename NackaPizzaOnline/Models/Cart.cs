7e7e46663d87b90ad21de87d1a6898858bef1a0e
Jesper Engdahl <jesperengdahl@hotmail.com>
2017-08-30 13:47:59 +0200

    Created a cart class

---
+++ NackaPizzaOnline/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NackaPizzaOnline.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        [Display(Name="Kundkorg")]
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        [Display(Name ="Summa")]
+        public int Sum { get; set; }
+        public bool IsLoggedIn { get; set; }
+        public string UserId { get; set; }
+    }
+}
